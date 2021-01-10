0357aee6724425650980f4f3f8fee9a41bdcbdeb
Abdallah <abdallahchaar90@gmail.comh>
2021-01-10 21:09:58 +0100

    träning om convert letters toLocaleLowerCase

---
+++ main.js
@@ -0,0 +1,11 @@
+/* global console, alert , prompt*/
+
+var myString = "I Love Js",
+
+mySmallString = myString.toLocaleLowerCase();
+
+console.log(myString);
+
+console.log(mySmallString);
+
+console.log("HELLO IAM OSAMA".toLocaleLowerCase());
